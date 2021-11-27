d56f52efd1957b1bfa22106a3fb1ca22bd19f00c
jahnvi51 <92022919+jahnvi51@users.noreply.github.com>
2021-11-27 11:39:43 +0000

    Initial commit

---
+++ product1.js
@@ -0,0 +1,14 @@
+const product = [
+    {
+        productId : 1,
+        title : "iwatch", 
+        price : 2000,
+        category  : "watch",
+        companyId : 1, 
+        sellerId : 1,
+    }
+
+];
+
+
+module.exports = product;
---
+++ seller1.js
@@ -0,0 +1,11 @@
+const seller = [
+    {
+        sellerId: 1,
+        name: "jahnvi",
+        productId : 1,
+    }
+
+];
+
+
+module.exports = seller;
